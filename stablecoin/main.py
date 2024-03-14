afc05c7fbd8c3d885ec29eb8c16a15ab34fdaa77
Boon Chong Peh <pehboonchong@gmail.com>
2024-03-14 22:33:16 +0800

    updated main

--- stablecoin/main.py
+++ stablecoin/main.py
@@ -114,6 +114,6 @@ def main(test_folders=None):
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     # os.chdir("stablecoin") # change directory for local test
-    main(test_folders=["usdt"])
+    # main(test_folders=["usdt"])
